64bfe36ec18b7e30e9bf05bb3b2719e982cc6b79
drz <qiyunzhu@gmail.com>
2020-07-11 01:08:36 -0700

    working progress

--- woltka/q2/plugin.py
+++ woltka/q2/plugin.py
@@ -17,17 +17,72 @@ Minimum requirement: QIIME 2 2019.1
     QIIME 2 since 2019.1.
 """
 
-import biom
+from io import StringIO
 
-from woltka.biom import profile_to_biom
-from woltka.workflow import workflow
+import pandas as pd
+import biom
+from skbio import TreeNode
+
+from woltka.workflow import (
+    workflow, parse_samples, build_mapper, classify as cwf)
+from woltka.table import prep_table
+from woltka.biom import table_to_biom
+from woltka.tree import read_lineage, fill_root
 
 
 def gotu(input_path: str) -> biom.Table:
     """Generate a gOTU table based on sequence alignments.
     """
     profile = workflow(input_path, None)['none']
-    return profile_to_biom(profile)
+    return table_to_biom(*prep_table(profile))
+
+
+def classify(input_path: str,
+             rank: str,
+             lineage: pd.Series = None,
+             sample_metadata: pd.DataFrame = None) -> biom.Table:
+    """Classify sequences based on their alignments to references through a
+    hierarchical classification system.
+    """
+    # available external compressors
+    zippers = {}
+
+    # parse input samples
+    samples, files, demux = parse_samples(input_path)
+
+    # build classification hierarchy
+    tree, rankdic, namedic, root = build_hierarchy(taxonomy=lineage)
+
+    # build mapping module
+    mapper, chunk = build_mapper()
+
+    # classify query sequences
+    profile = cwf(mapper=mapper,
+                  files=files,
+                  samples=samples,
+                  demux=demux,
+                  tree=tree,
+                  rankdic=rankdic,
+                  namedic=namedic,
+                  root=root,
+                  ranks=[rank],
+                  chunk=chunk,
+                  zippers=zippers)[rank]
+
+    return table_to_biom(*prep_table(profile))
+
+
+def build_hierarchy(taxonomy: pd.Series = None,
+                    phylogeny: TreeNode = None,
+                    mapping: pd.Series = None) -> (dict, dict, dict, dict):
+    tree, rankdic, namedic = {}, {}, {}
+
+    if taxonomy is not None:
+        tree, rankdic = read_lineage(StringIO(taxonomy.to_csv(
+            sep='\t', header=False)))
+
+    root = fill_root(tree)
+    return tree, rankdic, namedic, root
 
 
 def filter_values(table: biom.Table, th: float) -> biom.Table:
--- woltka/q2/plugin_setup.py
+++ woltka/q2/plugin_setup.py
@@ -8,11 +8,13 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from qiime2.plugin import Plugin, Str
+from qiime2.plugin import Plugin, Str, MetadataColumn, Categorical
+
 from q2_types.feature_table import FeatureTable, Frequency
+from q2_types.feature_data import FeatureData, Taxonomy
 
 from woltka import __version__
-from woltka.q2.plugin import gotu
+from woltka.q2.plugin import gotu, classify
 
 
 plugin = Plugin(
@@ -46,5 +48,40 @@ plugin.methods.register_function(
     name='gOTU table generation',
     description=('Generate a gOTU table based on sequence alignments against '
                  'a reference genome database.'),
+    citations=[]
+)
+
+
+plugin.methods.register_function(
+    function=classify,
+    inputs={
+        'lineage': FeatureData[Taxonomy],
+    },
+    input_descriptions={
+        'lineage': 'Lineage strings. Can accept Greengenes-style rank prefix.'
+    },
+    parameters={
+        'input_path': Str,
+        'rank': Str,
+        'alignments_mapping': MetadataColumn[Categorical]
+    },
+    parameter_descriptions={
+        'input_path': (
+            'Path to a multiplexed alignment file, or a directory of per-'
+            'sample alignment files.'),
+        'rank': (
+            'Classify sequences at this rank. Enter "none" to directly report '
+            'subjects; enter "free" for free-rank classification.'),
+        'alignments_mapping': 'Sample metadata'
+    },
+    outputs=[
+        ('table', FeatureTable[Frequency])
+    ],
+    output_descriptions={
+        'table': 'Output feature table.'
+    },
+    name='Flexible hierarchical sequence classifier',
+    description=('Classify sequences based on their alignments to references '
+                 'through a hierarchical classification system.'),
     citations=[]
 )
